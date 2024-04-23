6325d0ee294dafb9d6074988821e365f441f6852
akadjsam <akadjsam@gmail.com>
2024-04-24 02:13:32 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
+int main() {
+    // 첫 번째 문자열
+    string a = "234";
+    // 두 번째 문자열
+    std::string* b = new string("239");
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    // 메모리 누수를 방지하기 위해 동적으로 할당된 문자열을 삭제
+    delete b;
 
-//Modern Style (enum class c++ 11)
-enum class Java{ //기본적으로 enum은 integer이고 약한 결합이지만 enum class는 강한 결합임
-    PASS,
-    FAIL
-};
-
-int main(){
-    Java c = Java::PASS;
-
-    switch(c){
-        case Java::PASS: // PASS 대신에 0을 대입하면 에러가 발생 함. 강한 결합이기 때문
-            cout<< "합격" << endl;
-            break;
-        case Java::FAIL:
-            cout<< "재수강" << endl;
-            break;
-    }
     return 0;
 }
